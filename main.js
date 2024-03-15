306c98629753b7dbc113c75717394deb7f857ef0
hao-codes <59805422+hao-codes@users.noreply.github.com>
2024-03-15 14:11:18 +0100

    added form

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
-const myLibrary = [];
 
-function Book() {
+
+function Book(title, author, pages, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = readStatus;
+  // function to return book information
+  this.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus} yet.`;
+  }
   // the constructor...
 }
 
+const redRising = new Book("Red Rising", "Pierce Brown", 382, "finished reading")
+const goldenSon = new Book("Golden Son", "Pierce Brown", 464, "currently reading")
+const morningStar = new Book("Morning Star", 	"Pierce brown", 525, "not started")
+
+
+const myLibrary = [redRising, goldenSon, morningStar];
+
+
+console.log(myLibrary);
+
 function addBookToLibrary() {
   // do stuff here
-}
+
+};
+
+// add some default books to array to display
+
