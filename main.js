78e9af586807206802324c5e4dcfa56d23a69b56
hao-codes <59805422+hao-codes@users.noreply.github.com>
2024-03-22 14:57:15 +0100

    minor changes

--- main.js
+++ main.js
@@ -28,6 +28,8 @@ function addBookToLibrary() {
 };
 //const bookTable = document.getElementsByClassName("book-table");
 const bookTable = document.querySelector(".book-table");
+
+const addBookButton = document.querySelector("#add-book");
 function createBookOverview(books) {
   // loop through array
   // display books in div class
@@ -37,14 +39,16 @@ function createBookOverview(books) {
   let numberBooks = books.length;
   let nRows = Math.ceil(numberBooks / 4);
 
- 
+
   bookTable.innerHTML = "";
 
   for (let i = 0; i < numberBooks; i++) {
-    let bookCard = document.createElement("div");
-    bookCard.className = "bookcard";
-    bookCard.innerHTML = "Title: " + books[i].title + "Author: "+books[i].author + " Pages: "+ books[i].pages + " Reading Status: " + books[i].readStatus
-    bookTable.appendChild(bookCard);
+    let bookCardDiv = document.createElement("div");
+    bookCardDiv.className = "bookcard";
+    bookCardDiv.classList
+    bookCardDiv.innerHTML = "Title: " + books[i].title + "Author: " + books[i].author + " Pages: " + books[i].pages + " Reading Status: " + books[i].readStatus
+    
+    bookTable.appendChild(bookCardDiv);
   }
   // loop through book array 
 
@@ -54,4 +58,26 @@ function createBookOverview(books) {
 }
 createBookOverview(myLibrary);
 
+// create function to add book to array
 
+function addBook() {
+  // Get the input elements
+  const newTitle = document.getElementById("title");
+  const newAuthor = document.getElementById("author");
+  const newPages = document.getElementById("pages");
+  const newStatus = document.getElementById("readingStatus");
+
+
+  
+  // Add the values to the array
+  myLibrary.push({
+    title: newTitle,
+    author: newAuthor,
+    pages: newPages,
+    readStatus: newStatus
+  });
+  console.log(myLibrary);
+}
+
+console.log(myLibrary);
+addBookButton.addEventListener('click', addBook);
